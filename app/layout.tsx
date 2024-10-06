e31b612fe4f923298f8118c540055f4d7fd130ee
Anmol Jain <100015521+anmol562002@users.noreply.github.com>
2024-10-06 11:51:05 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
+import { ProModal } from "@/components/pro-modal";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FameFlow.ai",
+  description:
+    "From Thought to Talk: FameFlow.ai - Where Famous Figures Come Alive.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn("bg-secondary", inter.className )}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ProModal/>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
